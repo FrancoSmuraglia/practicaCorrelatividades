30d80814d5e9e20ab8b5e8db80e507030921960e
FrancoSmuraglia <fsmuraglia@frba.utn.edu.ar>
2024-04-03 16:12:30 -0300

    Final

--- src/test/java/domain/TestCorrelatividades.java
+++ src/test/java/domain/TestCorrelatividades.java
@@ -44,4 +44,23 @@ public class TestCorrelatividades {
         Inscripcion inscripcionFranco = franco.inscribirse(analisisMatematico2, fisica2);
         Assert.assertTrue(inscripcionFranco.aprobada());
     }
+    @Test
+    public void testDosMateriasUnaNoCumpleCorrelativas(){
+        Alumno franco = new Alumno("Franco", "Smuraglia", 2040803);
+        Materia analisisMatematico1 = new Materia("Analisis Matematico I");
+        Materia algebraYGeometriaAnalitica = new Materia("Algebra y Geometría Analítica");
+        Materia analisisMatematico2 = new Materia("Analisis Matemático II");
+        Materia fisica1 = new Materia("Física I");
+        Materia fisica2 = new Materia("Física II");
+
+        analisisMatematico2.agregarCorrelativa(analisisMatematico1);
+        analisisMatematico2.agregarCorrelativa(algebraYGeometriaAnalitica);
+        fisica2.agregarCorrelativa(fisica1);
+
+        franco.aprobarMateria(analisisMatematico1);
+        franco.aprobarMateria(algebraYGeometriaAnalitica);
+
+        Inscripcion inscripcionFranco = franco.inscribirse(analisisMatematico2, fisica2);
+        Assert.assertFalse(inscripcionFranco.aprobada());
+    }
 }
